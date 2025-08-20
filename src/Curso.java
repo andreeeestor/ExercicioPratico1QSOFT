b6507a9ce8a6cdd37a1eaec5180025bcc09b0308
andreeeestor <andrenestor0503@gmail.com>
2025-08-19 21:42:46 -0300

    first commit

---
+++ src/Curso.java
@@ -0,0 +1,42 @@
+import java.util.ArrayList;
+
+public class Curso {
+    private String name;
+    private String time;
+    private ArrayList<Disciplina> disciplines;
+
+    public Curso(String name, String time) {
+        this.name = name;
+        this.time = time;
+        this.disciplines = new ArrayList<>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public void addDiscipline(Disciplina discipline) {
+        disciplines.add(discipline);
+    }
+
+    public ArrayList<Disciplina> getDisciplines() {
+        return disciplines;
+    }
+
+    public void updateName(String newName) {
+        setName(newName);
+        System.out.println("Curso atualizado: " + newName);
+    }
+}
---
+++ src/Disciplina.java
@@ -0,0 +1,42 @@
+import java.security.PublicKey;
+
+public class Disciplina {
+    public String name;
+    private int workload;
+    private double grade;
+
+    public Disciplina(String name, int workload, double grade) {
+        this.name = name;
+        this.workload = workload;
+        this.grade = grade;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getWorkload() {
+        return workload;
+    }
+
+    public void setWorkload(int workload) {
+        this.workload = workload;
+    }
+
+    public double getGrade() {
+        return grade;
+    }
+
+    public void setGrade(double grade) {
+        this.grade = grade;
+    }
+
+    public void updateName(String newName) {
+        setName(newName);
+        System.out.println("Discipline updated: " + newName);
+    }
+}
